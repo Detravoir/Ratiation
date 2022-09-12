06992265feeba761f90dfdbabe2f0fe893cfa792
TortleTurtle <danielmeijs@gmail.com>
2022-09-12 22:12:29 +0200

    Upgrades now affect the spawner. Spawner has a chance of spawning shinies!

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Scriptable_Objects;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,14 +9,16 @@ public class GameManager : MonoBehaviour
     public static GameManager gameManager;
     public SpriteRenderer wall;
     
-    public GameObject rat_Prefab;
+    [SerializeField] private GameObject ratPrefab;
+    [SerializeField] private RatType[] ratTypes;
+    [SerializeField] private UpgradeType spawnRateUpgrade;
+    [SerializeField] private UpgradeType spawnChanceUpgrade;
 
     public float Timer = 0;
 
     void Awake()
     {
         gameManager = this;
-        
     }
 
     void Update()
@@ -25,16 +28,23 @@ public class GameManager : MonoBehaviour
         if (Timer <= 0f)
         {
             Spawn_Rat();
-            Timer = 10f;
-        }     
+            Timer = 10f - spawnRateUpgrade.Level; //decrease by a second per level.
+        }
     }
 
     public void Spawn_Rat()
     {
         //Pick a location to spawn the rat
         Vector2 position = new Vector2(Random.Range(wall.bounds.extents.x, (wall.bounds.extents.x * -1)), Random.Range(wall.bounds.extents.y, (wall.bounds.extents.y * -1)));
-             
+        
         //Spawn a rat
-        Instantiate(rat_Prefab, position, Quaternion.identity, null);            
+        Rat rat = Instantiate(ratPrefab, position, Quaternion.identity, null).GetComponent<Rat>();
+        
+        //See if its a shiney.
+        if (Random.Range(0f, 100f) > 100 - 10 * spawnChanceUpgrade.Level)
+        {
+            rat.type= ratTypes[1];
+            rat.Set_Rat();
+        }
     }
 }
