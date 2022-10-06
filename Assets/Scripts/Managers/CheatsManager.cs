c80a1e5b12e0356d1add2ff8b26ebeaf912d59d9
Detravoir <thomasdiondehaas@hotmail.com>
2022-10-06 15:31:51 +0200

    upgraded cheatscript

--- Assets/Scripts/Managers/CheatsManager.cs
+++ Assets/Scripts/Managers/CheatsManager.cs
@@ -22,9 +22,48 @@ public class CheatsManager : MonoBehaviour
     {
         cheatsInput = cheatsInputField.text;
 
-        if(cheatsInput == "CHEATCODE")
+        if(cheatsInput == "motherload")
         {
-            Debug.Log("cheat is gemaakt");
+            EventManager.GiveMoney += AddMoney;
+            Debug.Log("I AM RICH");
         }
+
+        if(cheatsInput == "gottacatchthemall")
+        {
+            EventManager.UnlockRatDex += UnlockRatDex;
+            Debug.Log("I caught them all!");
+        }
+
+        if( cheatsInput == "salesman")
+        {
+            EventManager.UnlockShop += UnlockShop;
+            Debug.Log("Now i can buy every rat!");
+        }
+
+        if(cheatsInput == "VERMINTIDE")
+        {
+            EventManager.VERMINTIDE += VERMINTIDE;
+            Debug.Log("VERMINTIDE");
+        }
+    }
+
+    private void AddMoney()
+    {
+        // add shit ton of money
+    }
+
+    private void UnlockRatDex()
+    {
+        // Fully unlock the rat dex
+    }
+
+    private void UnlockShop()
+    {
+        // Make every rat availbable in the shop
+    }
+
+    private void VERMINTIDE()
+    {
+        // VERMINTIDE
     }
 }
--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -9,4 +9,10 @@ public static class EventManager
     public static Action<double> OnCheeseGenerated;
 
     public static Action OnRatSpawn;
+
+    //Cheat events
+    public static Action GiveMoney;
+    public static Action UnlockRatDex;
+    public static Action UnlockShop;
+    public static Action VERMINTIDE;
 }
